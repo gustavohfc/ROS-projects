3f2c363a9596a5a48b8f035cdca89bee00845236
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-10-30 18:42:18 -0200

    Base do roteiro 7

---
+++ fcr2017/src/140021671_roteiro7.cpp
@@ -0,0 +1,16 @@
+#include <ros/ros.h>
+#include <ros/console.h>
+
+#include "common.h"
+#include "odometer.h"
+#include "laser_sensor.h"
+#include "motion_controller.h"
+#include "graph.h"
+
+
+#define LOOP_RATE 60
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
