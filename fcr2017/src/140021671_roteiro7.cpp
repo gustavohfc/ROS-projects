7391fd87862505d4ba24773e458700c1c886393b
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-10-31 10:31:05 -0200

    Roteiro 7

--- fcr2017/src/140021671_roteiro7.cpp
+++ fcr2017/src/140021671_roteiro7.cpp
@@ -14,5 +14,21 @@
 
 int main(int argc, char **argv)
 {
+    ros::init(argc, argv, "roteiro7");
+    ros::NodeHandle nodeHandle;
+
+    // Set verbosity level to debug
+    // if (ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug)) {
+    //     ros::console::notifyLoggerLevelsChanged();
+    // }
+
+
+    // Initialize objects
+    PioneerState current_state(WaitingGoal);
+    Odometer odometer(nodeHandle);
+    LaserSensor laser_sensor(nodeHandle);
+    Graph graph(argv[1], nodeHandle, odometer, laser_sensor);
+    MotionController motion_controller(nodeHandle, current_state, odometer, laser_sensor, graph);
+
     return 0;
 }
