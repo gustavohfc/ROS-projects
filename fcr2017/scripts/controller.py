8f928372ed3f4cc721972904665a42573408320b
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2017-11-15 18:45:20 -0200

    Python controller

---
+++ fcr2017/scripts/controller.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# pylint: disable=C0103
+
+import tty
+import sys
+from termios import tcflush, TCIFLUSH, tcsetattr, tcgetattr, TCSADRAIN
+from std_msgs.msg import Char
+import rospy
+
+INSTRUCTIONS = """
+    ^
+  Q W E
+< A   D >
+"""
+CTRL_C = 3
+valid_keys = ['a', 'q', 'w', 'e', 'd', 'r']
+
+
+if __name__ == "__main__":
+    pub = rospy.Publisher('controller', Char, queue_size=1)
+    rospy.init_node('controller', anonymous=True)
+    settings = tcgetattr(sys.stdin)
+    msg = Char()
+
+    print INSTRUCTIONS
+    tty.setraw(sys.stdin.fileno())
+
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        rate.sleep()
+
+        # Clear the input buffer
+        tcflush(sys.stdin, TCIFLUSH)
+
+        key = sys.stdin.read(1).lower()
+
+        # Verify if Ctrl C was pressed
+        if ord(key) == CTRL_C:
+            break
+
+        if key in valid_keys:
+            pub.publish(ord(key))
+        else:
+            pub.publish(0)
+
+    tcsetattr(sys.stdin, TCSADRAIN, settings)
